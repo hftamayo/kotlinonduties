8b24be750bad0016e5b4d140db739a58fc141274
hftamayo <hftamayo@gmail.com>
2024-01-17 15:17:57 -0600

    adding libraries to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    }
+}
 plugins {
     id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
