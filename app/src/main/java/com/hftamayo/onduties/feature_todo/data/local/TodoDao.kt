1ebbe6c2eb96e614b72a83b9bf16591017234da9
hftamayo <hftamayo@gmail.com>
2024-02-02 15:30:28 -0600

    Dao interface for local Todo data class

---
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/data/local/TodoDao.kt
@@ -0,0 +1,27 @@
+package com.hftamayo.onduties.feature_todo.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.hftamayo.onduties.feature_todo.data.local.dto.LocalTodoItem
+
+@Dao
+interface TodoDao {
+    @Query("SELECT * FROM todo")
+    fun getAllTodoItems(): List<LocalTodoItem>
+
+    @Query("SELECT * FROM todo WHERE id = :id")
+    suspend fun getTodoItemById(id: Int): LocalTodoItem?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addAllTodoItem(todos: List<LocalTodoItem>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addTodoItem(todo: LocalTodoItem): Long
+
+    @Delete
+    suspend fun deleteTodoItem(todo: LocalTodoItem)
+
+}
