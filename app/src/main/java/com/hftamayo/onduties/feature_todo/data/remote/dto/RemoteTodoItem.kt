58ce84b1e8a3593b31abb60cb745a459d040c12b
hftamayo <hftamayo@gmail.com>
2024-02-08 15:41:59 -0600

    adding field name for the data model

--- app/src/main/java/com/hftamayo/onduties/feature_todo/data/remote/dto/RemoteTodoItem.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/data/remote/dto/RemoteTodoItem.kt
@@ -1,10 +1,18 @@
 package com.hftamayo.onduties.feature_todo.data.remote.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class RemoteTodoItem(
+    @SerializedName("title")
     val title: String,
+    @SerializedName("description")
     val description: String,
+    @SerializedName("timestamp")
     val timestamp: Long,
+    @SerializedName("completed")
     val completed: Boolean,
+    @SerializedName("archived")
     val archived: Boolean,
+    @SerializedName("id")
     val id: Int?
 )
