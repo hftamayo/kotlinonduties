5e9cc9659a2daa9eac60b5ceb8c220dcadd13c54
hftamayo <hftamayo@gmail.com>
2024-02-08 15:53:07 -0600

    working on retrofit provider part 1

--- app/src/main/java/com/hftamayo/onduties/feature_todo/data/di/TodoModule.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/data/di/TodoModule.kt
@@ -9,11 +9,22 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object TodoModule {
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
     @Provides
     fun provideRoomDao(database: TodoDatabase): TodoDao {
         return database.dao
