7c031ef74a05a997e13a1adfd22f6534d3cb8a3b
hftamayo <hftamayo@gmail.com>
2024-02-09 12:27:35 -0600

    adding retrofit calls to TodoModule

--- app/src/main/java/com/hftamayo/onduties/feature_todo/data/di/TodoModule.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/data/di/TodoModule.kt
@@ -4,24 +4,31 @@ import android.content.Context
 import androidx.room.Room
 import com.hftamayo.onduties.feature_todo.data.local.TodoDao
 import com.hftamayo.onduties.feature_todo.data.local.TodoDatabase
+import com.hftamayo.onduties.feature_todo.data.remote.TodoApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object TodoModule {
 
+    @Provides
+    fun providesRetrofitApi(retrofit: Retrofit): TodoApi {
+        return retrofit.create(TodoApi::class.java)
+    }
+
     @Singleton
     @Provides
     fun providesRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("https://jsonplaceholder.typicode.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://todo-8ed5c.firebaseio.com/")
             .build()
     }
 
@@ -41,5 +48,4 @@ object TodoModule {
             TodoDatabase.DATABASE_NAME
         ).fallbackToDestructiveMigration().build()
     }
-    )
 }
