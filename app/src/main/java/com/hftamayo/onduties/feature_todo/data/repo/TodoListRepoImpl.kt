6075cf6350a44b85ba532ebe2ffc6a8d2be9083c
hftamayo <hftamayo@gmail.com>
2024-02-15 15:55:39 -0600

    working on TodoList Repository implementation

--- app/src/main/java/com/hftamayo/onduties/feature_todo/data/repo/TodoListRepoImpl.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/data/repo/TodoListRepoImpl.kt
@@ -1,41 +1,78 @@
 package com.hftamayo.onduties.feature_todo.data.repo
 
+import android.net.http.HttpException
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresExtension
 import com.hftamayo.onduties.feature_todo.data.local.TodoDao
+import com.hftamayo.onduties.feature_todo.data.mapper.toLocalTodoItem
+import com.hftamayo.onduties.feature_todo.data.mapper.toTodoItemListFromLocal
 import com.hftamayo.onduties.feature_todo.data.remote.TodoApi
 import com.hftamayo.onduties.feature_todo.domain.model.TodoItem
 import com.hftamayo.onduties.feature_todo.domain.repo.TodoListRepo
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import java.net.ConnectException
+import java.net.UnknownHostException
+import java.util.UnknownFormatConversionException
 
 class TodoListRepoImpl(
     private val dao: TodoDao,
     private val api: TodoApi,
-    private val dispatcher: CoroutineDispatcher
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+
 ): TodoListRepo {
     override suspend fun getAllTodos(): List<TodoItem> {
-        TODO("Not yet implemented")
+        getAllTodosFromRemote()
+        return dao.getAllTodoItems().toTodoItemListFromLocal()
     }
 
     override suspend fun getAllTodosFromLocalCache(): List<TodoItem> {
-        TODO("Not yet implemented")
+        return dao.getAllTodoItems().toTodoItemListFromLocal()
     }
 
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override suspend fun getAllTodosFromRemote() {
-        TODO("Not yet implemented")
+        return withContext(dispatcher) {
+            try {
+            } catch (e: Exception) {
+                when(e){
+                    is UnknownHostException, is ConnectException, is HttpException -> {
+                        Log.e("HTTP" "Error: No data from Remote")
+                        if()
+                        // handle network exception
+                    }
+                }
+                // handle exception
+            }
+            val remoteTodoList = api.getAllTodos()
+            dao.addTodoItems(remoteTodoList.map { it.toLocalTodoItem() })
+        }
+    }
+
+    private fun isCacheEmpty(): Boolean {
+        return dao.getAllTodoItems().isEmpty()
     }
 
     override suspend fun getSingleTodoItemById(id: Int): TodoItem? {
-        TODO("Not yet implemented")
+        return dao.getSingleTodoItemById(id)?.toTodoItem()
     }
 
     override suspend fun addTodoItem(todoItem: TodoItem) {
-        TODO("Not yet implemented")
+        val newId = dao.addTodoItem(todo.toLocalTodoItem())
+        val id = newId.toInt()
+        val url = "todo/$id.json"
+        api.addTodo(url, todo.toRemoteTodoItem().copy(id = id))
     }
 
+    override suspend fun updateTodoItem(todoItem: TodoItem) {
+        dao.addTodoItem(todo.toLocalTodoItem())
+        api.updateTodoItem(todo.id, todo.toRemoteTodoItem())
+
+    }
     override suspend fun deleteTodoItem(todoItem: TodoItem) {
         TODO("Not yet implemented")
     }
 
-    override suspend fun updateTodoItem(todoItem: TodoItem) {
-        TODO("Not yet implemented")
-    }
+
 }
