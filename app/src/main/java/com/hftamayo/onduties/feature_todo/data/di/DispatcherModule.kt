db365d14f5d17d2bdfbb5b60e9b4b365ecd1f369
hftamayo <hftamayo@gmail.com>
2024-02-15 15:54:47 -0600

    coding the first part of dispatcher module

--- app/src/main/java/com/hftamayo/onduties/feature_todo/data/di/DispatcherModule.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/data/di/DispatcherModule.kt
@@ -1,4 +1,15 @@
 package com.hftamayo.onduties.feature_todo.data.di
 
+import dagger.Provides
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DispatcherIO
 object DispatcherModule {
+    @IoDispatcher
+    @Provides
+    fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
