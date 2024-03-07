e73846ccf84a95b27d14ffa58d88de1585b19331
hftamayo <hftamayo@gmail.com>
2024-03-07 15:38:13 -0600

    interface class to interact with repository

--- app/src/main/java/com/hftamayo/onduties/feature_todo/domain/use_case/TodoUseCases.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/domain/use_case/TodoUseCases.kt
@@ -1,5 +1,6 @@
 package com.hftamayo.onduties.feature_todo.domain.use_case
 
+import com.hftamayo.onduties.core.util.TodoUseCaseStrings
 import com.hftamayo.onduties.feature_todo.domain.model.TodoItem
 import com.hftamayo.onduties.feature_todo.domain.repo.TodoListRepo
 import com.hftamayo.onduties.feature_todo.domain.util.InvalidTodoItemException
@@ -7,11 +8,34 @@ import javax.inject.Inject
 
 class TodoUseCases @Inject constructor(
     private val repo: TodoListRepo
-){
-    suspend fun addTodoItem(todo: TodoItem){
-        if(todo.title.isBlank() || todo.description.isBlank()){
-            throw InvalidTodoItemException()
+) {
+    suspend fun addTodoItem(todo: TodoItem) {
+        if (todo.title.isBlank() || todo.description.isBlank()) {
+            throw InvalidTodoItemException(TodoUseCaseStrings.EMPTY_TITLE_OR_DESCRIPTION)
         }
         repo.addTodoItem(todo)
+    }
+
+    suspend fun updateTodoItem(todo: TodoItem) {
+        if (todo.title.isBlank() || todo.description.isBlank()) {
+            throw InvalidTodoItemException(TodoUseCaseStrings.EMPTY_TITLE_OR_DESCRIPTION)
+        }
+        repo.updateTodoItem(todo)
+    }
+
+    suspend fun deleteTodoItem(todo: TodoItem) {
+        repo.deleteTodoItem(todo)
+    }
+
+    suspend fun toggleCompleteTodoItem(todo: TodoItem) {
+        repo.updateTodoItem(todo.copy(completed = !todo.completed))
+    }
+
+    suspend fun toggleArchiveTodoItem(todo: TodoItem) {
+        repo.updateTodoItem(todo.copy(archived = !todo.archived))
+    }
+
+    suspend fun getTodoItemById(id: Int): TodoItem? {
+        return repo.getSingleTodoItemById(id)
     }
 }
