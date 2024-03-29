0f057b4c6409b1068e7426eda6c5a36aa9629562
hftamayo <hftamayo@gmail.com>
2024-03-29 12:26:56 -0600

    updating gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("com.android.tools.build:gradle:8.3.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
     }
 }
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("com.android.library") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
+    id("com.android.library") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
 }
