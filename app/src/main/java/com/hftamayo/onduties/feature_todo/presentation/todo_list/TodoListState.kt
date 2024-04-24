a3efaa7078d319323e4ca25c1422d05dc9812699
Herbert Tamayo <hftamayo@gmail.com>
2024-04-24 14:56:32 -0600

    preparing the TodoListState with interaction with last action confirmation or rollback

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/TodoListState.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/TodoListState.kt
@@ -5,4 +5,7 @@ import com.hftamayo.onduties.feature_todo.domain.util.SortingDirection
 import com.hftamayo.onduties.feature_todo.domain.util.TodoItemOrder
 data class TodoListState (
     val todoItems: List<TodoItem> = emptyList(),
+    val todoItemOrder: TodoItemOrder = TodoItemOrder.Time(SortingDirection.Down, true),
+    val isLoading: Boolean = true,
+    val error: String? = null
 )
