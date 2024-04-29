d8692c8b58dd7489a3d6eadecb438d27548d6fe1
Herbert Tamayo <hftamayo@gmail.com>
2024-04-29 14:51:07 -0600

    event class for handling operations like sort, delete and toggle

---
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/TodoListEvent.kt
@@ -0,0 +1,12 @@
+package com.hftamayo.onduties.feature_todo.presentation.todo_list
+
+import com.hftamayo.onduties.feature_todo.domain.model.TodoItem
+import com.hftamayo.onduties.feature_todo.domain.util.TodoItemOrder
+
+sealed class TodoListEvent{
+    data class Sort(val todoItemOrder: TodoItemOrder): TodoListEvent()
+    data class Delete(val todoItem: TodoItem): TodoListEvent()
+    data class ToggleCompleted(val todoItem: TodoItem): TodoListEvent()
+    data class ToggleArchived(val todoItem: TodoItem): TodoListEvent()
+
+}
