eec581aa7e7f87cc36ebc5bf1ffcb5b35d321438
Herbert Tamayo <hftamayo@gmail.com>
2024-04-29 14:51:28 -0600

    adding a new string message

--- app/src/main/java/com/hftamayo/onduties/core/util/TodoConstants.kt
+++ app/src/main/java/com/hftamayo/onduties/core/util/TodoConstants.kt
@@ -3,3 +3,7 @@ package com.hftamayo.onduties.core.util
 object TodoUseCaseStrings {
     const val EMPTY_TITLE_OR_DESCRIPTION = "Both the Todo title and description must be populated"
 }
+
+object TodoListStrings {
+    const val COULD_NOT_RETRIEVE_TODO_ITEMS = "Could not retrieve Todo items: "
+}
