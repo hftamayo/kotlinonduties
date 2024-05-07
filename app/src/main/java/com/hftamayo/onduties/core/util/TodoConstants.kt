c83525a2a09b91ceb4bd4dd57c0990dbd209e412
Herbert Tamayo <hftamayo@gmail.com>
2024-05-07 14:38:36 -0600

    updating constraints

--- app/src/main/java/com/hftamayo/onduties/core/util/TodoConstants.kt
+++ app/src/main/java/com/hftamayo/onduties/core/util/TodoConstants.kt
@@ -7,3 +7,9 @@ object TodoUseCaseStrings {
 object TodoListStrings {
     const val COULD_NOT_RETRIEVE_TODO_ITEMS = "Could not retrieve Todo items: "
 }
+
+object ContentDescriptions {
+    const val DELETE_TODO_ITEM = "Delete Todo item"
+    const val COMPLETE_TODO_ITEM = "Complete Todo item"
+    const val ARCHIVE_TODO_ITEM = "Archive Todo item"
+}
