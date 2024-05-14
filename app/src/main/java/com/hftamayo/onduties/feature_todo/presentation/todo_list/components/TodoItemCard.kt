67c8c9bc56c6a2c9fe145cae9057f90894763eb1
Herbert Tamayo <hftamayo@gmail.com>
2024-05-14 15:06:55 -0600

    todoITemCard part1

---
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/TodoItemCard.kt
@@ -0,0 +1,13 @@
+package com.hftamayo.onduties.feature_todo.presentation.todo_list.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.hftamayo.onduties.feature_todo.domain.model.TodoItem
+
+@Composable
+fun TodoItemCard (
+    todo: TodoItem,
+    modifier: Modifier = Modifier,
+    onDeleteClick: () -> Unit,
+){
+}
