0bd6bb5bf36626640bcf36940d3f2e5c361aed6d
Herbert Tamayo <hftamayo@gmail.com>
2024-05-15 14:08:34 -0600

    todoITemCard part2

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/TodoItemCard.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/TodoItemCard.kt
@@ -2,12 +2,38 @@ package com.hftamayo.onduties.feature_todo.presentation.todo_list.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.hftamayo.onduties.feature_todo.domain.model.TodoItem
+import com.hftamayo.onduties.ui.theme.OnDutiesTheme
 
 @Composable
 fun TodoItemCard (
     todo: TodoItem,
     modifier: Modifier = Modifier,
     onDeleteClick: () -> Unit,
+    onCompleteClick: () -> Unit,
+    onArchiveClick: () -> Unit
+    onCardClick: () -> Unit
 ){
+}
+
+@Preview
+@Composable
+fun TodoItemCardPreview() {
+    OnDutiesTheme {
+        TodoItemCard(
+            todo = TodoItem(
+                title = "Kotlin Language",
+                description = "Keep Learning Kotlin Language",
+                timestamp = System.currentTimeMillis(),
+                completed = true,
+                archived = false,
+                id = 1
+            ),
+            onDeleteClick = {},
+            onCompleteClick = {},
+            onArchiveClick = {},
+            onCardClick = {}
+        )
+    }
 }
