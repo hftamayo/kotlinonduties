4c9b87f4a047133ed7d15e7c096f6e85352cb2e9
Herbert Tamayo <hftamayo@gmail.com>
2024-05-21 15:25:35 -0600

    sortingRow component part 2

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/SortingRow.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/SortingRow.kt
@@ -3,27 +3,40 @@ package com.hftamayo.onduties.feature_todo.presentation.todo_list.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.unit.sp
+import com.hftamayo.onduties.core.util.ContentDescriptions
 
 @Composable
-fun IconRow (
+fun IconRow(
     modifier: Modifier = Modifier,
     text: String,
     isChecked: Boolean,
-){
-    Row (
+) {
+    Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier.fillMaxWidth()
-    ){
+    ) {
         Text(
             text = text,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onSurface,
-            modifier = Modifier.padding(16.dp)
+            font = 24.sp,
+            lineHeight = 30.sp,
         )
+        if (isChecked) {
+            Icon(
+                imageVector = Icons.Default.Check,
+                contentDescription = ContentDescriptions.SELECTED,
+                tint = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        }
 
     }
 }
