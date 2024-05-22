2313ff3e8bc101a5461495687375a39121adf6d6
Herbert Tamayo <hftamayo@gmail.com>
2024-05-22 15:31:48 -0600

    sorting drawer options part2

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/SortingDrawerOptions.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/SortingDrawerOptions.kt
@@ -1,9 +1,25 @@
 package com.hftamayo.onduties.feature_todo.presentation.todo_list.components
 
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.runtime.Composable
+import com.hftamayo.onduties.feature_todo.domain.util.TodoItemOrder
 
 @Composable
-fun sortingDrawerOptions() {
+fun sortingDrawerOptions(
+    todoItemOrder: TodoItemOrder,
+    onOrderChange: (TodoItemOrder) -> Unit
+) {
+    val titleSelected = todoItemOrder::class == TodoItemOrder.Title::class
+    NavigationDrawerItem(
+        label = {
+                IconRow(
+                    text = , isChecked = )
+
+        },
+        selected = false,
+        onClick = {
+    onOrderChange(TodoItemOrder.Title(todoItemOrder.sortingDirection, todoItemOrder.showArchived))
+    })
 
 
 }
