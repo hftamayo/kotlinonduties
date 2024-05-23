b6424412c2d7a0b8c7703ef6c2ec67e057225664
Herbert Tamayo <hftamayo@gmail.com>
2024-05-23 15:26:42 -0600

    managing sortingMenuOptions part2

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/SortingDrawerOptions.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/SortingDrawerOptions.kt
@@ -1,7 +1,10 @@
 package com.hftamayo.onduties.feature_todo.presentation.todo_list.components
 
+import androidx.compose.material3.Divider
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.runtime.Composable
+import com.hftamayo.onduties.core.util.TodoListStrings
+import com.hftamayo.onduties.feature_todo.domain.util.SortingDirection
 import com.hftamayo.onduties.feature_todo.domain.util.TodoItemOrder
 
 @Composable
@@ -12,14 +15,92 @@ fun sortingDrawerOptions(
     val titleSelected = todoItemOrder::class == TodoItemOrder.Title::class
     NavigationDrawerItem(
         label = {
-                IconRow(
-                    text = , isChecked = )
-
+            IconRow(
+                text = TodoListStrings.TITLE,
+                isChecked = titleSelected
+            )
         },
         selected = false,
         onClick = {
-    onOrderChange(TodoItemOrder.Title(todoItemOrder.sortingDirection, todoItemOrder.showArchived))
-    })
+            onOrderChange(
+                TodoItemOrder.Title(
+                    todoItemOrder.sortingDirection,
+                    todoItemOrder.showArchived
+                )
+            )
+        }
+    )
 
+    val timeSelected = todoItemOrder::class == TodoItemOrder.Time::class
+    NavigationDrawerItem(
+        label = {
+            IconRow(
+                text = TodoListStrings.TIME,
+                isChecked = timeSelected
+            )
+        },
+        selected = false,
+        onClick = {
+            onOrderChange(
+                TodoItemOrder.Time(
+                    todoItemOrder.sortingDirection,
+                    todoItemOrder.showArchived
+                )
+            )
+        }
+    )
+
+    val completedSelected = todoItemOrder::class == TodoItemOrder.Completed::class
+    NavigationDrawerItem(
+        label = {
+            IconRow(
+                text = TodoListStrings.Completed,
+                isChecked = completedSelected
+            )
+        },
+        selected = false,
+        onClick = {
+            onOrderChange(
+                TodoItemOrder.Completed(
+                    todoItemOrder.sortingDirection,
+                    todoItemOrder.showArchived
+                )
+            )
+        }
+    )
+
+    Divider()
+
+    val sortDownSelected = todoItemOrder.sortingDirection == SortingDirection.Down
+    NavigationDrawerItem(
+        label = {
+            IconRow(
+                text = TodoListStrings.SORT_DOWN,
+                isChecked = sortDownSelected
+            )
+        },
+        selected = false,
+        onClick = {
+            onOrderChange(
+                TodoItemOrder.copy(SortingDirection.Down, todoItemOrder.showArchived)
+            )
+        }
+    )
+
+    val sortDownSelected = todoItemOrder.sortingDirection == SortingDirection.Down
+    NavigationDrawerItem(
+        label = {
+            IconRow(
+                text = TodoListStrings.SORT_DOWN,
+                isChecked = sortDownSelected
+            )
+        },
+        selected = false,
+        onClick = {
+            onOrderChange(
+                TodoItemOrder.copy(SortingDirection.Down, todoItemOrder.showArchived)
+            )
+        }
+    )
 
 }
