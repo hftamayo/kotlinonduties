481c607ff4f64e0ae125c8059b1e1ad3569bee7d
Herbert Tamayo <hftamayo@gmail.com>
2024-06-04 15:43:13 -0600

    adding rotation config

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/TodoListScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.navigation.NavController
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -18,5 +19,8 @@ fun TodoListScreen (
     val snackbarHostState = remember { SnackbarHostState() }
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+
+    val configuration = LocalConfiguration.current
+    val isPortrait = configuration.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT
 
 }
