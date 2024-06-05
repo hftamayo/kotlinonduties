a970ee51c7bdcb8498e60952c1dd505e8c5b8e00
Herbert Tamayo <hftamayo@gmail.com>
2024-06-05 15:26:08 -0600

    adding buttons and other scaffold for actions

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/TodoListScreen.kt
@@ -1,26 +1,100 @@
 package com.hftamayo.onduties.feature_todo.presentation.todo_list
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.hftamayo.onduties.R
+import com.hftamayo.onduties.core.util.TodoListStrings
 
 @Composable
-fun TodoListScreen (
+fun TodoListScreen(
     navController: NavController,
     viewModel: TodoListViewModel = hiltViewModel()
-    ){
+) {
     val state = viewModel.state.value
     val snackbarHostState = remember { SnackbarHostState() }
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
     val configuration = LocalConfiguration.current
-    val isPortrait = configuration.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT
+    val isPortrait =
+        configuration.orientation == android.content.res.Configuration.ORIENTATION_PORTRAIT
+
+    val backgroundImage = if (isPortrait) {
+        R.drawable.background_portrait
+    } else {
+        R.drawable.background_landscape
+    }
+
+    LaunchedEffect(key1 = true) {
+        viewModel.getTodoItems()
+    }
+
+    ModalNavigationDrawer(drawerContent = (
+            drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(
+                    text = TodoListStrings.SORT_BY,
+                    modifier = Modifier.padding(16.dp),
+                    fontSize = 34.sp,
+                    lineHeight = 38.sp
+                )
+                Divider()
+                SortingDrawerOptions(
+                    todoItemOrder = state.todoItemOrder,
+                    onOrderChange = { order ->
+                        viewModel.onEvent(TodoListEvent.Sort(order))
+                    }
+                )
+            }
+            //TODO
+        }
+    ) {
+        Scaffold(
+            floatingActionButton = {
+                FloatingActionButton(onClick = {
+                /*TODO*/
+                },
+                    shape = CircleShape,
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = ContentDescriptions.ADD_TODO_ITEM,
+                        tint = MaterialTheme.colorScheme.onPrimary
+                    )
+
+                }
+            }
+        ) {
+
+        }
+
+     }) {
+
+    }
 
 }
