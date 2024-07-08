66dc60585765cd23cda0039e858c35697ab065a5
Herbert Tamayo <hftamayo@gmail.com>
2024-07-08 15:45:04 -0600

    updating MainActivity code

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/MainActivity.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/MainActivity.kt
@@ -14,19 +14,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OnDutiesTheme {
-                MyApp()
+            TodoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
             }
         }
     }
 }
 
-@Composable
-fun MyApp() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-
-    }
-}
