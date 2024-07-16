5df0108a8300a5161e292c423f620452d4810c47
Herbert Tamayo <hftamayo@gmail.com>
2024-07-16 09:38:25 -0600

    adding a missed import

--- app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/SortingRow.kt
+++ app/src/main/java/com/hftamayo/onduties/feature_todo/presentation/todo_list/components/SortingRow.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
